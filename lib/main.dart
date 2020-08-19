aa3875e0301f1ac7dcfa2e6e8c707a46d84d6065
Vishnu Suresh <vishnusuresh19012000@gmail.com>
2020-08-19 22:59:23 +0530

    online

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    print("Server Url ${ServerApi.url}");
+    print("Server Url ${ServerApi.offlineOnline} ${ServerApi.url}");
     return MultiProvider(
       providers: bloc,
       child: MaterialApp(
